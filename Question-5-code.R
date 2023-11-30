89a0254c1ace8654a2130d3372ea1e62449a341d
Willbill1978 <150149267+Willbill1978@users.noreply.github.com>
2023-11-30 11:41:39 +0000

    Update Question-5-code.R
    added a few comments

--- Question-5-code.R
+++ Question-5-code.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 
-Virus_data<-read.csv("question-5-data/Cui_etal2014.csv")
-nrow(Virus_data)
-ncol(Virus_data)
+Virus_data<-read.csv("question-5-data/Cui_etal2014.csv")#read in the data in csv form
+nrow(Virus_data)#number of rows
+ncol(Virus_data)#number of columns
 
 #if the data follows V=BL^a
 #To linearise we need to 
@@ -11,7 +11,7 @@ ggplot(aes(Genome.length..kb. , Virion.volume..nm.nm.nm.),data=Virus_data)+
   scale_x_continuous(trans="log")+
   scale_y_continuous(trans="log")
 
-model1<-lm(log(Virion.volume..nm.nm.nm.) ~ log(Genome.length..kb.), Virus_data)
+model1<-lm(log(Virion.volume..nm.nm.nm.) ~ log(Genome.length..kb.), Virus_data)#turns into linear model
 summary(model1)
 #Intercept is is 7.0748 with p value of 2.28e-10
 #Gradient is  1.5152 with p Value of 6.44e-10
