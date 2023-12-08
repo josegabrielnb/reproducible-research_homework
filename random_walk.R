33bb398e136edbbd41ba246887fc371585f46418
assignmentcontrol <153260107+assignmentcontrol@users.noreply.github.com>
2023-12-08 02:13:32 -0800

    Add files via upload

---
+++ random_walk.R
@@ -0,0 +1,45 @@
+# install.packages("ggplot2")
+install.packages("gridExtra")
+
+library(ggplot2)
+library(gridExtra)
+
+random_walk <- function(n_steps, seed) {
+  set.seed(seed)
+  
+  df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
+  
+  df[1,] <- c(0, 0, 1)
+  
+  for (i in 2:n_steps) {
+    h <- 0.25
+    
+    angle <- runif(1, min = 0, max = 2 * pi)
+    
+    df[i, 1] <- df[i - 1, 1] + cos(angle) * h
+    df[i, 2] <- df[i - 1, 2] + sin(angle) * h
+    df[i, 3] <- i
+  }
+  
+  return(df)
+}
+
+set.seed(50)
+
+data1 <- random_walk(500, 50)
+
+plot1 <- ggplot(aes(x = x, y = y), data = data1) +
+  geom_path(aes(colour = time)) +
+  theme_bw() +
+  xlab("x-coordinate") +
+  ylab("y-coordinate")
+
+data2 <- random_walk(500, 50)
+
+plot2 <- ggplot(aes(x = x, y = y), data = data2) +
+  geom_path(aes(colour = time)) +
+  theme_bw() +
+  xlab("x-coordinate") +
+  ylab("y-coordinate")
+
+grid.arrange(plot1, plot2, ncol = 2)
