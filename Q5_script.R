25339875bfcdda04ace5029c2b979f41b73dc0c2
flamingobadger <rebecca.mccash@pmb.ox.ac.uk>
2024-12-10 16:26:03 +0000

    Update Q5_script.R

--- Q5_script.R
+++ Q5_script.R
@@ -24,11 +24,11 @@ summary(linear_model)
 #plotting data with log transformation
 
 ggplot(data = virus_data, aes(x = log(Genome.length..kb.), y = log(Virion.volume..nm.nm.nm.)))+
-  geom_point() +
-  geom_smooth(method = lm) +
-  labs(x = "log[Genome length (kb)]", y = "log[Virion volume (nm3)]" ) +
-  theme_bw() +
-  theme(axis.title.x = element_text(size = 12, face = "bold"),
-        axis.title.y = element_text(size = 12, face = "bold"))
+  geom_point() + #adding data observations as points
+  geom_smooth(method = lm) + #adding fitted regression line
+  labs(x = "log[Genome length (kb)]", y = "log[Virion volume (nm3)]" ) + #adding axes labels
+  theme_bw() + #aesthetics
+  theme(axis.title.x = element_text(size = 12, face = "bold"), #making x-axis title look nice
+        axis.title.y = element_text(size = 12, face = "bold"))  #making y-axis title look nice
 
 
